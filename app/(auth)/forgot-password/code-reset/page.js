e9c080a58629c81d0eae29ae061e199a93d75e8a
naufandarmawan <naufandarmawan@gmail.com>
2024-05-21 17:18:10 +0700

    folder structure setup

---
+++ app/(auth)/forgot-password/code-reset/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CodeReset = () => {
+  return (
+    <div>CodeReset</div>
+  )
+}
+
+export default CodeReset
---
+++ app/(main)/recipes/[id]/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProductDetails = ({params}) => {
+
+  return (
+    <div>ProductDetails - {params.id}</div>
+  )
+}
+
+export default ProductDetails
